9ed6cb304496d7163b8825676d09240f527f2954
ted <tvolchko@gmail.com>
2024-06-20 19:46:15 -0500

    Small styling, page title added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/homePage';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Eddyville Schedules";
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
+      <title>Eddyville Schedules</title>
         <HomePage/>
       </header>
       
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <title>Eddyville Schedules</title>
     <App />
   </React.StrictMode>
 );
