08588c5f96b76433405f5c0b9e869ce1b5f44577
ted <tvolchko@gmail.com>
2024-06-20 21:01:41 -0500

    Corrected MetaData

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import HomePage from './components/homePage';
 import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 function App() {
-  useEffect(() => {
-    document.title = "Eddyville Schedules";
-  }, [])
+  // useEffect(() => {
+  //   document.title = "Eddyville Schedules";
+  // }, [])
   return (
     <div className="App">
+      <Helmet>
+        <title>Eddyville Schedules</title>
+        <meta name="title" content="Eddyville Schedules" />
+        <meta name="description" content="Cargill Eddyville Crew Schedules" />
+      </Helmet>
       <header className="App-header">
-      <title>Eddyville Schedules</title>
+
         <HomePage/>
       </header>
       
